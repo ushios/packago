2b8b0005a86540cabd69715b4996cdba8f9f7303
UshioShugo <ushio.s@gmail.com>
2015-12-08 18:16:10 +0900

    add constructor.

--- animals/human.go
+++ animals/human.go
@@ -9,3 +9,12 @@ func (h Human) FullName() (fullname string) {
 	fullname = h.FirstName + " " + h.LastName
 	return
 }
+
+func NewHuman(firstName string, lastName string) Human {
+	human := Human{
+		FirstName: firstName,
+		LastName:  lastName,
+	}
+
+	return human
+}
--- animals/human_test.go
+++ animals/human_test.go
@@ -8,7 +8,19 @@ func TestHuman(t *testing.T) {
 		LastName:  "Nobita",
 	}
 
-	if human.FullName() != "Nobi Nobita" {
+	fullName := human.FullName()
+
+	if fullName != "Nobi Nobita" {
+		t.Errorf("human fullname want (%s)", "Nobi Nobita")
+	}
+}
+
+func TestNewHuman(t *testing.T) {
+	human := NewHuman("Nobi", "Nobita")
+
+	fullName := human.FullName()
+
+	if fullName != "Nobi Nobita" {
 		t.Errorf("human fullname want (%s)", "Nobi Nobita")
 	}
 }
